66d96f360aea05d36df343f4a97a4ae739e3dd56
Ed Preston <epreston@prestonsoft.com>
2023-11-05 21:55:09 +1100

    three 157
        "three": "0.157.0"
    remove unused vite glsl plugin
    simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
 // vite.config.js
 
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import glsl from 'vite-plugin-glsl';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  /** @type {import('vite').UserConfig} */
-  return {
-    // config options
-    assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.m4a', '**/*.hdr'],
-    plugins: [glsl({ compress: mode === 'production' ? true : false }), splitVendorChunkPlugin()],
-    build: {
-      // minify: false,
-      // assetsInlineLimit: 0,
-      target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4']
-    }
-  };
+export default defineConfig({
+  assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.hdr'],
+  plugins: [splitVendorChunkPlugin()],
+  build: {
+    // minify: false,
+    // assetsInlineLimit: 0,
+    target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4']
+  }
 });
