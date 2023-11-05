ddd32e1eb802671d23e2d13c1d849ba47aa721b1
Ed Preston <epreston@prestonsoft.com>
2023-11-05 23:59:41 +1100

    Update vite.config.js
    dep size observability

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,29 @@
 // vite.config.js
 
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.glb', '**/*.hdr'],
-  plugins: [splitVendorChunkPlugin()],
   build: {
     // minify: false,
     // assetsInlineLimit: 0,
-    target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4']
+    target: ['es2022', 'chrome112', 'edge112', 'firefox112', 'safari16.4', 'ios16.4'],
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        // size observability
+        manualChunks: (id) => {
+          // create chunk for three example deps.
+          if (id.includes('node_modules/three/examples')) {
+            return 'three-examples';
+          }
+          // create chunk for three core deps.
+          if (id.includes('node_modules/three') || id.includes('@three')) {
+            return 'three';
+          }
+        }
+      }
+    }
   }
 });
